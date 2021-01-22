978b6a8ec179869d7536fb2ba74c2d0b4cddecc8
FranklysG <franklys@appmake.com.br>
2021-01-22 08:33:37 -0300

    first push the application

---
+++ app/control/public/ViewFooter.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ViewFooter
+ *
+ * @version    1.0
+ * @package    control
+ * @subpackage public
+ * @author     Franklys Guimarães
+ * @copyright  Copyright (c) 2006 Adianti Solutions Ltd. (http://www.adianti.com.br)
+ * @license    http://www.adianti.com.br/framework-license
+ */
+class ViewFooter extends TPage
+{
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // replace the main section variables to section footer
+        $footer = new THtmlRenderer('app/pages/view_footer.html');
+        $footer->enableSection('main', array());
+
+        // add the template to the page
+        parent::add($footer);
+    }
+}
---
+++ app/control/public/body/ViewIndex.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ViewIndex
+ *
+ * @version    1.0
+ * @package    control
+ * @subpackage public
+ * @author     Franklys Guimarães
+ * @copyright  Copyright (c) 2006 Adianti Solutions Ltd. (http://www.adianti.com.br)
+ * @license    http://www.adianti.com.br/framework-license
+ */
+class ViewIndex extends TPage
+{
+    public function __construct()
+    {
+        parent::__construct();
+       
+        
+        // replace the main section variables to section body
+        $body = new THtmlRenderer('app/pages/view_index.html');
+        $body->enableSection('main', array());
+        
+        
+        // add the template to the page
+        parent::add(new ViewHeader);
+        parent::add($body);
+        parent::add(new ViewFooter);
+    }
+}
